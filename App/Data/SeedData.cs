0607be71eda34dba2f51ce67771845a6907f7d82
pedro <pedro@calculum.ai>
2023-10-14 21:34:33 -0400

    Add project files.

---
+++ App/Data/SeedData.cs
@@ -0,0 +1,48 @@
+﻿using App.Data;
+using App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace App.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                // Validate
+                if (context.Product == null)
+                {
+                    throw new ArgumentNullException(nameof(context));
+                }
+
+                // If there are any existing products, return to avoid seeding
+                if (context.Product.Any())
+                {
+                    return; // Database has already been seeded
+                }
+
+                var random = new Random();
+
+                // Generate 100 sample products with random quantities and prices and add them to the database
+                for (int i = 1; i <= 100; i++)
+                {
+                    context.Product.Add(new Product
+                    {
+                        Name = $"Product {i}",
+                        Description = $"Description for Product {i}",
+                        Quantity = random.Next(0, 1001), // Random quantity between 0 and 1000
+                        Price = Math.Round((decimal)(random.NextDouble() * (100 - 0.01) + 0.01), 2) // Random price between 0.01 and 100
+                    });
+                }
+
+                // Save changes to the database
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ App/Migrations/20231013160229_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
